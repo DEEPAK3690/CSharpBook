61ac59b4cfa151fb58ab60d5be8a77727e708163
DEEPAK3690 <deepakpalani0@gmail.com>
2023-09-19 12:43:39 +0530

    changes

--- CSharpBook/Program.cs
+++ CSharpBook/Program.cs
@@ -3,86 +3,89 @@ using CSharpBook;
 using CSharpBook.SOLID;
 using static CSharpBook.EnumsProblem;
 
-Problem problem = Problem.LINQ;
+//Problem problem = Problem.LINQ;
 
-if (problem == Problem.LINQ)
+foreach (Problem problem in Enum.GetValues(typeof(Problem)))
 {
-    LINQ lINQ = new LINQ();
-    LINQEnums subLINQProblem = LINQEnums.Grup;
+    if (problem == Problem.LINQ)
+    {
+        LINQ lINQ = new LINQ();
+        LINQEnums subLINQProblem = LINQEnums.Grup;
 
-    if (subLINQProblem == LINQEnums.Query)
-        lINQ.QueryExpression();
-    if (subLINQProblem == LINQEnums.ProjectionOperation)
-        lINQ.ProjectionOperation();
-    if (subLINQProblem == LINQEnums.SortingData)
-        lINQ.SortingData();
-    if (subLINQProblem == LINQEnums.Join)
-        lINQ.Joins();
-    if (subLINQProblem == LINQEnums.Grup)
-        lINQ.Grup();
-}
-
-if (problem == Problem.Workout)
-{
-    WorkOut objWorkOut = new WorkOut();
-    objWorkOut.WorkoutSample3();
-}
-
-if (problem == Problem.AbstractClass)
-{
-    AbstractClassExample abstractClassExample = new AbstractClassExample();
-    abstractClassExample.MainMethod();
-}
-else if (problem == Problem.Inheritance)
-{
-    InheritanceExample inheritanceExample = new InheritanceExample();
-    inheritanceExample.MainMethod();
-}
-else if (problem == Problem.Delegates)
-{
-    Delegates delegates = new Delegates();
-    delegates.MainMethod();
-}
-else if (problem == Problem.AsyncAwait)
-{
-    AsyncAwaitExample asyncAwaitExample = new AsyncAwaitExample();
-    asyncAwaitExample.MainMethod();
-}
-else if (problem == Problem.Events)
-{
-    EventExample eventsExample = new EventExample();
-    eventsExample.MainMethod();
-}
-else if (problem == Problem.SOLIDDesignPrinciples)
-{
-    SOLIDPrinciples principleProblem = SOLIDPrinciples.SOLID_S;
-    if (principleProblem == SOLIDPrinciples.SOLID_S)
-    {
-        SingleResponsibilityPrinciple singleResponsibilityPrinciple = new SingleResponsibilityPrinciple();
-        singleResponsibilityPrinciple.MainMethod();
-    }
-    else if (principleProblem == SOLIDPrinciples.SOLID_O)
-    {
-        OpenClosePrinciple openCloseObj = new OpenClosePrinciple();
-        openCloseObj.MainMethod();
-    }
-    else if (principleProblem == SOLIDPrinciples.SOLID_L)
-    {
-
-    }
-    else if (principleProblem == SOLIDPrinciples.SOLID_I)
-    {
-        InterfaceSegregationPrinciple interfaceSegregationPrinciple = new InterfaceSegregationPrinciple();
-        interfaceSegregationPrinciple.MainMethod();
-    }
-    else if (principleProblem == SOLIDPrinciples.SOLID_D)
-    {
-        DependencyInversionPrinciple dependencyObj = new DependencyInversionPrinciple();
-        dependencyObj.MainMethod();
+        if (subLINQProblem == LINQEnums.Query)
+            lINQ.QueryExpression();
+        if (subLINQProblem == LINQEnums.ProjectionOperation)
+            lINQ.ProjectionOperation();
+        if (subLINQProblem == LINQEnums.SortingData)
+            lINQ.SortingData();
+        if (subLINQProblem == LINQEnums.Join)
+            lINQ.Joins();
+        if (subLINQProblem == LINQEnums.Grup)
+            lINQ.Grup();
     }
 
-}
-else
-{
-    Console.WriteLine("NONE");
+    if (problem == Problem.Workout)
+    {
+        WorkOut objWorkOut = new WorkOut();
+        objWorkOut.WorkoutSample3();
+    }
+
+    if (problem == Problem.AbstractClass)
+    {
+        AbstractClassExample abstractClassExample = new AbstractClassExample();
+        abstractClassExample.MainMethod();
+    }
+    else if (problem == Problem.Inheritance)
+    {
+        InheritanceExample inheritanceExample = new InheritanceExample();
+        inheritanceExample.MainMethod();
+    }
+    else if (problem == Problem.Delegates)
+    {
+        Delegates delegates = new Delegates();
+        delegates.MainMethod();
+    }
+    else if (problem == Problem.AsyncAwait)
+    {
+        AsyncAwaitExample asyncAwaitExample = new AsyncAwaitExample();
+        asyncAwaitExample.MainMethod();
+    }
+    else if (problem == Problem.Events)
+    {
+        EventExample eventsExample = new EventExample();
+        eventsExample.MainMethod();
+    }
+    else if (problem == Problem.SOLIDDesignPrinciples)
+    {
+        SOLIDPrinciples principleProblem = SOLIDPrinciples.SOLID_S;
+        if (principleProblem == SOLIDPrinciples.SOLID_S)
+        {
+            SingleResponsibilityPrinciple singleResponsibilityPrinciple = new SingleResponsibilityPrinciple();
+            singleResponsibilityPrinciple.MainMethod();
+        }
+        else if (principleProblem == SOLIDPrinciples.SOLID_O)
+        {
+            OpenClosePrinciple openCloseObj = new OpenClosePrinciple();
+            openCloseObj.MainMethod();
+        }
+        else if (principleProblem == SOLIDPrinciples.SOLID_L)
+        {
+
+        }
+        else if (principleProblem == SOLIDPrinciples.SOLID_I)
+        {
+            InterfaceSegregationPrinciple interfaceSegregationPrinciple = new InterfaceSegregationPrinciple();
+            interfaceSegregationPrinciple.MainMethod();
+        }
+        else if (principleProblem == SOLIDPrinciples.SOLID_D)
+        {
+            DependencyInversionPrinciple dependencyObj = new DependencyInversionPrinciple();
+            dependencyObj.MainMethod();
+        }
+
+    }
+    else
+    {
+        Console.WriteLine("NONE");
+    }
 }
